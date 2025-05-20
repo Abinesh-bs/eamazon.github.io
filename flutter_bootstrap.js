940acce12bc3d49a94f6728bca15cfca01955752
Abinesh Bs <abineshnesh07@gmai.lcom>
2025-05-20 16:13:57 +0530

    review with images

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1410416328"
+    serviceWorkerVersion: "2791992195"
   }
 });
