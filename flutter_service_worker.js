2794399d2667a2930e317ccb6f92bbbe9ac01577
Abinesh Bs <abineshnesh07@gmai.lcom>
2025-05-27 11:39:30 +0530

    Scrollable for categories

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -30,14 +30,14 @@ const RESOURCES = {"assets/AssetManifest.bin": "ef6e451c9c9b5c80d7add6a713b61dc1
 "favicon.png": "eecb98fa2ef66568df7f15e79e098ab8",
 "firebase-messaging-sw.js": "2c90b5b71a353409d0e4f56f36c20040",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "b6a6a19fac07930d954bb893261a3d5e",
+"flutter_bootstrap.js": "af73e06d4f4028c87bdb4126b24d5007",
 "icons/Icon-192.png": "160cb1718a25868fb4fa72f1514ff13a",
 "icons/Icon-512.png": "9be0f9e926dd38888fced08aa42cb7cf",
 "icons/Icon-maskable-192.png": "160cb1718a25868fb4fa72f1514ff13a",
 "icons/Icon-maskable-512.png": "9be0f9e926dd38888fced08aa42cb7cf",
 "index.html": "17364d9daf8b012c9b5e07b9576bbfcb",
 "/": "17364d9daf8b012c9b5e07b9576bbfcb",
-"main.dart.js": "37ee349a67f018203f3ed2feae840fe7",
+"main.dart.js": "ea042d9d1bc9b92dd819d6702ad97357",
 "manifest.json": "b3822b0689716c50fec36c8bdec686e0",
 "version.json": "6f0a89a25dc509ff73a0b096cc4a86b4"};
 // The application shell files that are downloaded before a service worker can
